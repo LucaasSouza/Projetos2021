5c5b98b6f5552c8dffcb92d308c033b820e943ed
LucasSouza <82621071+LucasSouza2021@users.noreply.github.com>
2021-04-26 11:32:17 -0300

    Update LoginConta.js

--- LoginConta.js
+++ LoginConta.js
@@ -1,74 +1,76 @@
-var contas = []
+var contas = [
+    {
+        email: "teste@gmail.com",
+        senha: "000",
+        nickName: "Testeuau",
+        idadeP: 18,
+    }
+]
 
 alert("Bem vindo ao 'Teste de Login'")
-alert("ATENÇÃO!!! \nPara acessar sua conta, é necessário criar a conta antes")
+alert("ATENÇÃO!!! \nPara acessar sua conta, é necessário criar a conta antes.")
 
-const idadePermMin = 12
-const idadePermMax = 110
+var idade = Number(prompt("Insira sua idade:"))
+if (idade >= 12 && idade <= 110) {
+    menu()
+} else {
+    alert("Idade não permitida!")
+}
 
-var idade = Number(prompt("Insira sua idade"))
+function menu() {
+    var menu = Number(prompt("Você deseja acessar \n1- Criar uma conta \n2- Acessar sua conta"))
 
-if (idade >= idadePermMin && idade <= idadePermMax) {
-    function newPerfil() {
-        var novoPerfil = {
-            email: prompt("Insira aqui seu email:"),
-            senha: prompt("Insira aqui sua senha:"),
-            nickName: prompt("Insira aqui seu Nickname:"),
-            idadeP: idade
+    switch (menu) {
+        case 1:
+            newPerfil()
+            break;
+
+        case 2:
+            testeLogin()
+            break;
+
+
+        default:
+            alert("Opção não encontrada!")
+            break;
+    }
+}
+
+function newPerfil() {
+    var novoPerfil = {
+        email: prompt("Insira aqui seu email:"),
+        senha: prompt("Insira aqui sua senha:"),
+        nickName: prompt("Insira aqui seu Nickname:"),
+        idadeP: idade
+    }
+    contas.push(novoPerfil)
+    menu()
+}
+
+function testeLogin() {
+    var flag = false
+    var emailLogin = prompt("Insira aqui seu email:");
+
+    for (i = 0; i < contas.length && flag === false; i++) {
+        if (emailLogin == contas[i].email) {
+            alert("Email correto!")
+
+            var senhaLogin = prompt("Insira aqui sua senha:")
+
+            if (senhaLogin == contas[i].senha) {
+                flag = true
+                alert("Login efetuado")
+
+                console.log(contas)
+            } else {
+                alert("Senha incorreta!")
+                menu()
+            }
         }
+    }
 
-        contas.push(novoPerfil)
+    if(flag == false) {
+        alert("Email incorreto!")
         menu()
     }
-
-    function testeLogin() {
-        var emailLogin = prompt("Insira aqui seu email:")
-
-        for (var i = 0; i < contas.length; i++) {
-            if (emailLogin == contas[i].email) {
-                alert("Email correto!")
-
-                var senhaLogin = prompt("Insira aqui sua senha:")
-                if (senhaLogin == contas[i].senha) {
-                    alert("Senha correta!")
-                    alert("Login uau")
-
-                    console.log("Lista de perfis: " + contas)
-                } else {
-                    alert("Senha incorreta!")
-                    menu()
-                }
-            } else {
-                alert("Email incorreto!")
-                menu()
-            }
-
-            if (emailLogin != contas[i].email) {
-                alert("Email não encontrado!")
-                menu()
-            }
-        }
-    }
-
-    menu()
-
-    function menu() {
-        var menu = Number(prompt("Você deseja acessar \n1- Criar uma conta \n2- Acessar sua conta"))
-
-        switch (menu) {
-            case 1:
-                newPerfil()
-                break;
-
-            case 2:
-                testeLogin()
-                break;
-
-            default:
-                alert("Opção não encontrada!")
-                break;
-        }
-    }
-}else{
-    alert("Idade não permitida!")
 }
